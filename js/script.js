d149b88430b6157176313d2e4a4a305078f5b52f
alexolivet <alexolivet@yahoo.com>
2017-10-03 12:51:41 +0200

    adding material design files

--- js/script.js
+++ js/script.js
@@ -43,74 +43,78 @@ $("#colors").hide();
 
 //search function starts here
 function doSearch() {
-    $("#result").show(); // result div to show when making new search
-    resultD.innerHTML = "";
-    var searchString = searchField.value;
-    if (searchString !== "") {
-        search(searchString).done(function(data) {
-            for (var artObj in data.artObjects) {
-                var rImg = document.createElement("img"); // create the image
-                rImg.src = data.artObjects[artObj].webImage.url; // the source of the image element is the url from rijks api
-                rImg.setAttribute("crossOrigin", "Anonymous"); //needed so I can actually copy the image for later use
-                rImg.setAttribute("class", "imageClass"); // set image class to allow css
+        $("#result").show(); // result div to show when making new search
+        resultD.innerHTML = "";
+        var searchString = searchField.value;
+        if (searchString !== "") {
+            search(searchString).done(function(data) {
+                for (var artObj in data.artObjects) {
+                    var rImg = document.createElement("img"); // create the image
+                    rImg.src = data.artObjects[artObj].webImage.url; // the source of the image element is the url from rijks api
+                    rImg.setAttribute("crossOrigin", "Anonymous"); //needed so I can actually copy the image for later use
+                    rImg.setAttribute("class", "imageClass"); // set image class to allow css
 
 
-                // rImg.onload = function(){ // need to get the image width and height therefore using onload
-                //   // Specify image dimensions for each image
-                //   $('img').each(function() {
-                //     var findImgWidth = $(this).width();
-                //     var findImgHeight = $(this).height();
-                //     $(this).attr('width', findImgWidth);
-                //     $(this).attr('height', findImgHeight);
-                //   });
-                // }
-                var link = document.createElement("a"); // create the link
-                link.appendChild(rImg); // append image to link
-                resultD.appendChild(link); // append link with image to div
-                link.setAttribute('href', data.artObjects[artObj].webImage.url); // set link path
-                link.setAttribute('class', 'anchor'); // set link class needed for on event which allows you to deal with dynamic data 
-                link.setAttribute("title", data.artObjects[artObj].title); // set text attribute of each image in text attribute
-                // link.href = "www.example.com"; //can be done this way too
+                    // rImg.onload = function(){ // need to get the image width and height therefore using onload
+                    //   // Specify image dimensions for each image
+                    //   $('img').each(function() {
+                    //     var findImgWidth = $(this).width();
+                    //     var findImgHeight = $(this).height();
+                    //     $(this).attr('width', findImgWidth);
+                    //     $(this).attr('height', findImgHeight);
+                    //   });
+                    // }
+                    var link = document.createElement("a"); // create the link
+                    link.appendChild(rImg); // append image to link
+                    resultD.appendChild(link); // append link with image to div
+                    link.setAttribute('href', data.artObjects[artObj].webImage.url); // set link path
+                    link.setAttribute('class', 'anchor'); // set link class needed for on event which allows you to deal with dynamic data 
+                    link.setAttribute("title", data.artObjects[artObj].title); // set text attribute of each image in text attribute
+                    // link.href = "www.example.com"; //can be done this way too
 
 
-                var title = document.createElement("p"); //create paragraph for the title
-                title.innerHTML = data.artObjects[artObj].title; // this is the title from rijks api
-                resultD.appendChild(title); // append link with image to div
+                    var title = document.createElement("p"); //create paragraph for the title
+                    title.innerHTML = data.artObjects[artObj].title; // this is the title from rijks api
+                    resultD.appendChild(title); // append link with image to div
 
-                $("#result img").each(function(i, image) { //for each image create a different id
-                    image.id = "image" + (i + 1);
-                });
-                $("#result a").each(function(i, anchor) { //for each anchor set the following attributes:
-                    //anchor.id = "anchor" + (i + 1);
-                    anchor.setAttribute('onclick', 'return false;'); // return false needed so image does not jump when clicked
-                    anchor.setAttribute('data-id', +(i + 1)); // set different data-id attribute needed for on event
-                });
-                // $("#result p").each(function (i, title){ //for each anchor set the following attributes:
-                // title.id = "anchor" + (i + 1);
-                // title.setAttribute('class', 'anchor');
-                //   title.setAttribute('data-id', + (i + 1)); // set different data-id attribute needed for on event
-                // });
-
-                setInterval(function() { //timeout starts
-                    $('img').trigger('load', function() {
-                        // need to get the image width and height therefore using onload
-                        //   // Specify image dimensions for each image
-
-                        $('img').each(function() {
-                            var findImgWidth = $(this).width();
-                            var findImgHeight = $(this).height();
-                            $(this).attr('width', findImgWidth);
-                            $(this).attr('height', findImgHeight);
-
-                        });
+                    $("#result img").each(function(i, image) { //for each image create a different id
+                        image.id = "image" + (i + 1);
                     });
-                }, 5000); //timeout ends
-                resultD.innerHTML += "<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>";
-            }
-        });
-    }
-} //search function ends here
+                    $("#result a").each(function(i, anchor) { //for each anchor set the following attributes:
+                        //anchor.id = "anchor" + (i + 1);
+                        anchor.setAttribute('onclick', 'return false;'); // return false needed so image does not jump when clicked
+                        anchor.setAttribute('data-id', +(i + 1)); // set different data-id attribute needed for on event
+                    });
+                    // $("#result p").each(function (i, title){ //for each anchor set the following attributes:
+                    // title.id = "anchor" + (i + 1);
+                    // title.setAttribute('class', 'anchor');
+                    //   title.setAttribute('data-id', + (i + 1)); // set different data-id attribute needed for on event
+                    // });
 
+                    setInterval(function() { //timeout starts
+                        $('img').trigger('load', function() {
+                            // need to get the image width and height therefore using onload
+                            //   // Specify image dimensions for each image
+
+                            $('img').each(function() {
+                                var findImgWidth = $(this).width();
+                                var findImgHeight = $(this).height();
+                                $(this).attr('width', findImgWidth);
+                                $(this).attr('height', findImgHeight);
+
+                            });
+                        });
+                    }, 5000); //timeout ends
+                    resultD.innerHTML += "<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>";
+                }
+            });
+        }
+    } //search function ends here
+
+    function rgbToHex(value) { //query to convert rgb to hex format
+                a = value
+                return "#" + ((1 << 24) + (+a[0] << 16) + (+a[1] << 8) + +a[2]).toString(16).slice(1)
+            };
 
 //You want to utilize the .on event with jquery which allows you to deal with dynamic data which is basically what you are dealing with.
 //Then when you want to deal with the data from that particular record/image you would call it as such.
@@ -136,12 +140,9 @@ $(document).on('click', '.anchor', 'img', function() {
         var color = colorThief.getPalette(img, 10); // color palette - number of colors returned
         var newHTML = $.map(color, function(value) { // rgb value returned are then placed in csss inline style
 
-            function rgbToHex(value) {//query to convert rgb to hex format
-                a = value
-                return "#" + ((1 << 24) + (+a[0] << 16) + (+a[1] << 8) + +a[2]).toString(16).slice(1)
-            }
+        rgbToHex;//get the hex code from value
 
-            console.log(rgbToHex(value));
+        //console.log(rgbToHex(value));
 
             return ('<div class="col-4" style="background-color:rgb(' + value.join(', ') + ')">' + rgbToHex(value) + '</div>'); // return the colors in hex format into the grid cell
         });
